c5ae2e2e34ec751a6f97d4ad537887a4c878cc6c
再见亦是路人 <15252114322@163.com>
2021-11-25 14:57:42 +0800

    fix: fix style

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,11 +1,13 @@
-use actix_web::web;
 use actix_files::Files;
-
+use actix_web::web;
 
 mod handler;
 
 pub(super) fn route(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/upload").route(web::get().to(handler::upload)).route(web::post().to(handler::save_file)))
-        .service(Files::new("/ex", "/root/ex").show_files_listing());
-    
+    cfg.service(
+        web::resource("/upload")
+            .route(web::get().to(handler::upload))
+            .route(web::post().to(handler::save_file)),
+    )
+    .service(Files::new("/ex", "/root/ex").show_files_listing());
 }
--- src/user/mod.rs
+++ src/user/mod.rs
@@ -10,7 +10,11 @@ pub fn route(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/user")
             .service(web::resource("/register").route(web::post().to(register)))
-            .service(web::resource("/login").route(web::post().to(login)).route(web::get().to(login_html)))
+            .service(
+                web::resource("/login")
+                    .route(web::post().to(login))
+                    .route(web::get().to(login_html)),
+            )
             .service(web::resource("/logout").route(web::get().to(logout)))
             .service(web::resource("/me").route(web::get().to(me))),
     );
